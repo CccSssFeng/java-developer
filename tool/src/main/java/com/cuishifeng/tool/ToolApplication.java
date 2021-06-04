7ce37fcc6e7a5d82ab7e9cd9a1c8dd53a0d4463f
cuishifeng <cuishifeng0207@163.com>
2021-06-04 09:38:39 +0800

    # 新建目录

---
+++ tool/src/main/java/com/cuishifeng/tool/ToolApplication.java
@@ -0,0 +1,16 @@
+package com.cuishifeng.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cuishifeng
+ */
+@SpringBootApplication
+public class ToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolApplication.class, args);
+    }
+
+}
